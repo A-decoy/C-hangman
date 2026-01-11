9faebae209bf1645d6948929e1d793204b48ef5d
A-decoy <sarasokheynasaroeun@gmail.com>
2026-01-11 13:02:00 +1000

    first commmit

---
+++ README.md
@@ -0,0 +1,10 @@
+Hangman C not finised but is working :)))).
+
+if try to debug or the word is too hard uncomment the code on line 14 it will print the word so can test if the code is working.
+
+will be given the prompt
+
+Ex. _ _ _ _ (trap)
+
+code will ask for input (please only put letters lowercase)
+
---
+++ main.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int getWord();
+
+char global_realWord[99];
+char global_hide[99];
+char input;
+char list_input[99] =" ";
+
+int main(){
+    int tries = 0;
+    int digits;
+    char temp[99] = "\0";
+    
+    getWord();
+
+    // printf("%s", global_realWord);
+
+    digits = strlen(global_realWord);
+    
+    for(int i = 0; i < digits; i++){
+        global_hide[i] = '_';
+    }
+
+    strcpy(temp, global_hide);
+    
+    while(tries < 8 && strcmp(global_hide, global_realWord) != 0){
+        
+        for(int i = 0; i < digits; i++){
+            printf("%c ", global_hide[i]);
+        }
+
+        if(tries > 0){
+            printf("\nInputed: ");
+            for(int i = 0; i < ((sizeof(list_input)/sizeof(list_input[0])) - 1); i++){
+                printf("%c ", list_input[i + 1]);
+            }
+        }
+
+        printf("\nNumber of tries: %d", (8 - tries));
+        
+        while(true){
+            printf("\nEnter letter: ");
+            scanf(" %c", &input);
+            if(strchr(list_input, input) == NULL){
+                break;
+            }
+            else{
+                printf("You've have already guessed this answer. ");
+                continue;
+            }
+        }
+
+        list_input[strlen(list_input)] = input;
+
+        for(int i = 0; i < digits; i++){
+            if(input == global_realWord[i]){
+                global_hide[i] = input;
+            }
+        }
+        
+        if(strcmp(temp, global_hide) == 0){
+        tries++;
+        }
+
+        strcpy(temp, global_hide);
+
+    }
+
+    if(tries == 8){
+        printf("sorry the man died :(\n");
+    }
+    else{
+        printf("Congratuation! You got the word right!\n");
+    }
+
+    for(int i = 0; i < digits; i++){
+        printf("%c ", global_realWord[i]);
+    }
+}
+
+int getWord(){
+    
+    char word[99];
+    char **list = NULL;
+    int count = 0;
+
+    FILE *pFile = fopen("word_list.txt", "r");
+
+    if(pFile == 0){
+        printf("Could not open file\n");
+        return 1;
+    }
+
+    while(fscanf(pFile, "%99s", word) == 1){
+        list = realloc(list, (count + 1) * sizeof(char *));
+        list[count] = malloc(strlen(word) + 1);
+        strcpy(list[count], word);
+        count++;
+    }
+
+    fclose(pFile);
+
+    srand(time(NULL));
+    strcpy(global_realWord, list[(rand() % (count - 1))]);
+
+    free(list);
+}
+
+
+
